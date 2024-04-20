534b68cf3efccc8e5820711d1ac85185466db4c0
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-04-20 13:44:15 +0200

    deletes .eslintignore and add script to eslint.config file instead

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,15 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: { globals: globals.browser },
+    ignores: [
+      './node_modules',
+      './package-lock.json',
+      './dist',
+      // ... any additional patterns to ignore
+    ],
+  },
   pluginJs.configs.recommended,
 ];
