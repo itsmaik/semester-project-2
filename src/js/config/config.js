30a2c1276aa8d4f821d054594f9edfeaffef056a
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-05-02 10:37:40 +0200

    Updates script for config fie

--- src/js/config/config.js
+++ src/js/config/config.js
@@ -1,2 +1,2 @@
-export const apiKey = import.meta.env.VITE_API_KEY;
-export const baseApiUrl = import.meta.env.VITE_API_URL;
+export const API_KEY = import.meta.env.VITE_API_KEY;
+export const BASE_API_URL = import.meta.env.VITE_API_URL;
