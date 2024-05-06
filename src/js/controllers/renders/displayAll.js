e3705a5167bbf04123f5d1435f21d19de5b216e6
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-05-06 21:40:42 +0200

    adds updated displayAll.js with pagination btns functionality

--- src/js/controllers/renders/displayAll.js
+++ src/js/controllers/renders/displayAll.js
@@ -1,13 +1,20 @@
 import { ListingsServices } from '../../services/ListingsServices';
 import { createAuctionCard } from '../templates/auctionCard';
 
-async function displayAllListings() {
-  const allListingsContainer = document.querySelector('.all-listings');
+const allListingsContainer = document.querySelector('.all-listings');
+const nextPage = document.getElementById('next-page');
+const pageNumbers = document.querySelectorAll('.page-number');
+const prevPage = document.getElementById('prev-page');
+
+let currentPage = 1;
+
+async function displayAllListings(page) {
   if (!allListingsContainer) return;
 
+  allListingsContainer.innerHTML = '';
+
   try {
-    const listings = await ListingsServices.getAllListingsPage(1, 12);
-    console.log(listings);
+    const listings = await ListingsServices.getAllListingsPage(page, 12);
 
     if (listings && listings.length > 0) {
       allListingsContainer.innerHTML = listings.map(createAuctionCard).join('');
@@ -15,6 +22,50 @@ async function displayAllListings() {
   } catch (error) {
     console.error('Error displaying Listings:', error);
   }
+
+  updateNavigation();
 }
 
-document.addEventListener('DOMContentLoaded', displayAllListings);
+function updateNavigation() {
+  let startPage = Math.max(1, currentPage - 1); // Start from current page - 1 unless it's less than 1
+  if (currentPage <= 2) {
+    startPage = 1;
+  }
+
+  pageNumbers.forEach((el, index) => {
+    el.textContent = startPage + index;
+    el.classList.toggle('disabled', currentPage === parseInt(el.textContent)); // Disable the current page
+  });
+
+  prevPage.classList.toggle('disabled', currentPage === 1); // Disable Previous on first page
+  nextPage.classList.toggle('disabled', currentPage === 10); // Assume 10 is the last page for example
+}
+
+prevPage.addEventListener('click', () => {
+  if (currentPage > 1) {
+    currentPage--;
+    displayAllListings(currentPage);
+  }
+});
+
+nextPage.addEventListener('click', () => {
+  if (currentPage < 10) {
+    // Assume 10 is the last page for example
+    currentPage++;
+    displayAllListings(currentPage);
+  }
+});
+
+pageNumbers.forEach((page) => {
+  page.addEventListener('click', (e) => {
+    const selectedPage = parseInt(e.target.textContent);
+    if (selectedPage !== currentPage) {
+      currentPage = selectedPage;
+      displayAllListings(currentPage);
+    }
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () =>
+  displayAllListings(currentPage),
+);
