447d99272659c3abccd682b9f69a2aebcb3036de
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-05-08 14:03:45 +0200

    removes unnecessary comments

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  //root: 'src',  //sets the root directory for the Vite project
   server: {
-    // server-specific configurations
     open: true, // automatically open the app in the browser on server start
     hot: true, // enables hot module replacement (HMR)
   },
   build: {
     outDir: 'dist',
   },
-  // include other configurations here as needed
 });
