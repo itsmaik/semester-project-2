9dd6cf02afa4d56aded23eb5586b43505653848c
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-05-11 00:01:06 +0200

    Updates UI of auction cards

--- src/js/controllers/templates/auctionCard.js
+++ src/js/controllers/templates/auctionCard.js
@@ -9,19 +9,26 @@ export function createAuctionCard(listing) {
     listing.media && listing.media.length > 0
       ? listing.media[0].alt
       : 'Default Image';
+  const description =
+    listing.description && listing.description.length > 0
+      ? listing.description
+      : 'No description provided.';
 
   return `
       <div class="col-md-4 mt-3 mb-3">
           <div class="card auction-card">
               <img src="${imageUrl}" class="card-img-top" alt="${imageAlt}">
-              <div class="card-body">
+              <div class="card-body relative p-0">
+                <div class="p-2 p-lg-4">
                   <h5 class="card-title">${listing.title}</h5>
-                  <p class="card-text">${listing.description}</p>
+                  <p class="card-text">${description}</p>
                   <a href="/src/pages/single-item/?id=${listing.id}" class="btn btn-primary mt-3">More Info</a>
-                  <div class="card-footer mt-5">
-                    <p class="card-text mt-2">Bids: ${String(listing._count.bids)}</p>
-                    <small class="fst-italic">End date: ${timestampConverter(listing.endsAt)}</small>
-                  </div>
+                </div>
+                  
+                <div class="mt-4 absolute bg-secondary px-2 px-lg-4 py-2 py-lg-3">
+                  <p class="card-text mt-2">Bids: ${String(listing._count.bids)}</p>
+                  <small class="fst-italic">End date: ${timestampConverter(listing.endsAt)}</small>
+                </div>
               </div>
           </div>
       </div>
