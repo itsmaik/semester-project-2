982453f989b6d85cade35db500f2ac236c340c48
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-05-16 20:09:33 +0200

    adds logic for displaying profile

---
+++ src/js/controllers/renders/displayProfile.js
@@ -0,0 +1,34 @@
+import { AuthServices } from '../../services/AuthServices';
+import { updateProfileController } from '../actions/profile';
+import { createProfileCard } from '../templates/profileCard';
+
+const profileDetailsContainer = document.querySelector('.profile-details');
+
+async function displayProfile() {
+  if (!profileDetailsContainer) return;
+  profileDetailsContainer.innerHTML = '';
+
+  const currentLoggedProfile = await AuthServices.getCurrentUser();
+
+  try {
+    const profileDetails = await AuthServices.getLoggedProfile(
+      currentLoggedProfile.name,
+    );
+
+    if (profileDetails) {
+      const singleListingHtml = createProfileCard(
+        profileDetails,
+        currentLoggedProfile,
+      );
+      profileDetailsContainer.innerHTML = singleListingHtml;
+    }
+
+    updateProfileController();
+  } catch (error) {
+    console.error('Error displaying profile:', error);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  displayProfile();
+});
