43d61e43b6981e5b6028487fafe56610c8fde604
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-05-16 23:39:07 +0200

    Adds newListingController

---
+++ src/js/controllers/actions/newListing.js
@@ -0,0 +1,37 @@
+import { ListingsServices } from '../../services/ListingsServices.js';
+
+document.addEventListener('DOMContentLoaded', function () {
+  newListingController();
+});
+
+export async function newListingController() {
+  const newListingForm = document.querySelector('#newListingModal form');
+  const title = newListingForm.querySelector('input#title');
+  const description = newListingForm.querySelector('input#description');
+  const image = newListingForm.querySelector('input#image-url');
+  const endsAt = newListingForm.querySelector('input#event-date');
+
+  newListingForm.querySelector('button').addEventListener('click', async () => {
+    if (!title && !endsAt) {
+      alert('Create a title and set an end date!');
+      return;
+    }
+
+    const newListingData = {
+      title: title.value ?? '',
+      description: description.value ?? '',
+      media: image.value
+        ? [
+            {
+              url: image.value,
+              alt: 'Listing Image',
+            },
+          ]
+        : [],
+      endsAt: endsAt.value ?? new Date(),
+    };
+
+    await ListingsServices.createListing(newListingData);
+    window.location.reload();
+  });
+}
