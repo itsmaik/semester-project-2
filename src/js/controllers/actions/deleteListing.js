61ec9b342186ab2deb0c40cb17072a462c742d51
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-05-16 23:40:52 +0200

    Includes delete function

---
+++ src/js/controllers/actions/deleteListing.js
@@ -0,0 +1,10 @@
+import { ListingsServices } from '../../services/ListingsServices.js';
+
+export async function deleteListingController(listing) {
+  const deleteButton = document.querySelector('.delete-listing-btn');
+
+  deleteButton.addEventListener('click', async () => {
+    await ListingsServices.deleteListing(listing.id);
+    window.location.href = '/';
+  });
+}
