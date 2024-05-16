4e3ade391b44d34c1f842201bd3cbe0fb1ce71a0
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-05-16 10:21:44 +0200

    changes date order to dd mm yyyy

--- src/js/utils/functions/timestampConverter.js
+++ src/js/utils/functions/timestampConverter.js
@@ -5,7 +5,7 @@ export default function timestampConverter(timestamp) {
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
 
-  const readableDate = `${month}-${day}-${year}`;
+  const readableDate = `${day}-${month}-${year}`;
 
   return readableDate;
 }
