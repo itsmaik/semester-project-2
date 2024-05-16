c25371e6e31576759b40865efecfbf32a56feab6
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-05-16 10:36:34 +0200

    updates btn behavior for edit, delete and bid btns

--- src/js/controllers/actions/logged.js
+++ src/js/controllers/actions/logged.js
@@ -8,18 +8,23 @@ export function updateButtonVisibility() {
   // const isLoggedIn = !!localStorage.getItem('accessToken');
 
   if (AuthServices.getAccessToken()) {
-    console.log(AuthServices.getAccessToken());
     document.querySelector('.login-modal-btn').style.display = 'none';
     document.querySelector('.register-modal-btn').style.display = 'none';
     document.querySelector('.logout-btn').style.display = 'inline-block';
     document.querySelector('.profile-btn').style.display = 'inline-block';
 
     //single-item button behavior
-    document.querySelector('#bidButton').removeAttribute('data-bs-target');
-    document.querySelector('#bidButton').removeAttribute('data-bs-toggle');
-    document.querySelector('#bidButton').classList.add('bidButton');
-    document.querySelector('#edit-btn').classList.remove('d-none');
-    document.querySelector('#delete-btn').classList.remove('d-none');
+    const bidButton = document.querySelector('#bidButton');
+    if (bidButton) {
+      bidButton.removeAttribute('data-bs-target');
+      bidButton.removeAttribute('data-bs-toggle');
+      bidButton.classList.add('bidButton');
+    }
+    document.querySelector('#edit-btn') &&
+      document.querySelector('#edit-btn').classList.remove('d-none');
+
+    document.querySelector('#delete-btn') &&
+      document.querySelector('#delete-btn').classList.remove('d-none');
   } else {
     document.querySelector('.profile-btn').style.display = 'none';
     document.querySelector('.logout-btn').style.display = 'none';
@@ -27,7 +32,7 @@ export function updateButtonVisibility() {
 }
 
 document.querySelector('.logout-btn').addEventListener('click', function () {
-  localStorage.removeItem('accessToken');
+  localStorage.clear();
   updateButtonVisibility();
   window.location.reload();
 });
