80bfb1e8d605a64a4a6e5f9727e1dedd6339c437
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-05-16 23:29:00 +0200

    Updates to display listings by profile

--- src/js/controllers/renders/displayProfile.js
+++ src/js/controllers/renders/displayProfile.js
@@ -1,8 +1,14 @@
 import { AuthServices } from '../../services/AuthServices';
+import { ListingsServices } from '../../services/ListingsServices';
+import { newListingController } from '../actions/newListing';
 import { updateProfileController } from '../actions/profile';
 import { createProfileCard } from '../templates/profileCard';
+import { createProfileListingsCard } from '../templates/profileListingsCard';
 
 const profileDetailsContainer = document.querySelector('.profile-details');
+const profileListingsContainer = document.querySelector(
+  '.profile-listings-container',
+);
 
 async function displayProfile() {
   if (!profileDetailsContainer) return;
@@ -15,6 +21,10 @@ async function displayProfile() {
       currentLoggedProfile.name,
     );
 
+    const profileListings = await ListingsServices.listingsByProfile(
+      currentLoggedProfile.name,
+    );
+
     if (profileDetails) {
       const singleListingHtml = createProfileCard(
         profileDetails,
@@ -23,7 +33,13 @@ async function displayProfile() {
       profileDetailsContainer.innerHTML = singleListingHtml;
     }
 
+    if (profileListings) {
+      const profileListingCardHtml = createProfileListingsCard(profileListings);
+      profileListingsContainer.innerHTML = profileListingCardHtml;
+    }
+
     updateProfileController();
+    newListingController();
   } catch (error) {
     console.error('Error displaying profile:', error);
   }
