d66562e55018ab5a3f1a1267a7aa140172fc0d46
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-05-18 23:32:11 +0200

    changes order of import for modals

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 import 'bootstrap';
 import '../sass/main.scss';
-import '../js/modals/loginModal';
-import '../js/modals/registerModal';
 import '../js/controllers/auth/loginController.js';
 import '../js/controllers/auth/registerController.js';
 import './controllers/renders/displayHome.js';
 import './controllers/renders/displayAll.js';
 import './controllers/renders/displaySingleListing.js';
 import './controllers/renders/displayProfile.js';
+import '../js/modals/registerModal';
+import '../js/modals/loginModal';
