73d7c3e0b3edc5844744593228c2f82eeb3409d2
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-05-18 19:30:23 +0200

    updates vite.config to include base to fix bug o n github pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/semester-project-2/', // Set this to your repository name
   server: {
     open: true, // automatically open the app in the browser on server start
     hot: true, // enables hot module replacement (HMR)
