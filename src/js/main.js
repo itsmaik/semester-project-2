267c55d51acea091af859a5a265f203df0881c90
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-05-18 23:46:03 +0200

    change import order on main.js and script place on index.html for testing

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import 'bootstrap';
 import '../sass/main.scss';
-import '../js/controllers/auth/loginController.js';
-import '../js/controllers/auth/registerController.js';
+// import '../js/controllers/auth/loginController.js';
+// import '../js/controllers/auth/registerController.js';
 import './controllers/renders/displayHome.js';
 import './controllers/renders/displayAll.js';
 import './controllers/renders/displaySingleListing.js';
