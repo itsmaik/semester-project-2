cfb3aabb35806d610c86a63f08d92df54cf974e9
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-05-21 13:42:21 +0200

    updates vite.config.js to include building of all pages instead of only main index

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  //base: '/semester-project-2/', // Set this to your repository name
   server: {
     open: true, // automatically open the app in the browser on server start
     hot: true, // enables hot module replacement (HMR)
   },
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        listings: '/src/pages/listings/index.html',
+        profile: '/src/pages/profile/index.html',
+        singleItem: '/src/pages/single-item/index.html',
+      },
+    },
   },
 });
